b7334e18657b1384a29d42a2f8ee77f794972595
Przemyslaw Bak <przemolicc@poczta.fm>
2019-02-18 16:40:11 +0000

    Final changes

--- nmapjson.py
+++ nmapjson.py
@@ -14,9 +14,33 @@ except ImportError as error:
     print("\n\nMissing python 3 library: '{0}' - please install it with i.e. 'pip3 install {0}'\n\n".format(error.name))
     sys.exit(1)
 
+class Envs:
+    
+    def readIniFile(self):
+        self.config.read(os.path.splitext(__file__)[0] + '.ini') # base name + 'ini'
 
-environments = {}
+    def __init__(self):
+        self.config = configparser.ConfigParser()
+        self.readIniFile()
+        self.environments = self.config._sections
+        for section in self.config.sections():
+            keys = self.config.options(section)
 
+    def rereadIniFile(self):
+        self.readIniFile()
+    
+    def getEnvironmentsList(self):
+        self.envList = []
+        for env in self.environments.items():
+            self.envList.append(env[0])
+        return self.envList
+        
+    def getUrlForEnvironment(self, envname):
+        return self.environments[envname]['url']
+
+    def getEnvDescription(self, envname):
+        return self.environments[envname]['description']
+        
 class ItemTable(Table):
     serial = Col('Serial')
     platform_version = Col('Platform version', column_html_attrs={'align': 'right'},)
@@ -24,24 +48,8 @@ class ItemTable(Table):
     host = Col('Host')
     port = Col('Port')
 
-def read_environments():
-    environments.clear() # you can update the ini file while running this app
-    config = configparser.ConfigParser()
-    config.read(os.path.splitext(__file__)[0] + '.ini') # base name + 'ini'
-    for section in config.sections():
-        environments[section] = config[section]['url']
-
-def build_help_output():
-    read_environments()
-    help_output = dict()
-    help_output = ['Click one of the following environments: ']
-    for key, _ in environments.items():
-        help_output.append("<a href={}>{}</a> ".format(request.url_root + 'env/' + key, key))
-    help_output += '<p>'
-    return help_output
-
 def help():
-    return render_template("help.html", environments=environments)
+    return render_template("help.html", environments=environments.getEnvironmentsList())
 
 app = Flask(__name__)
 app.add_url_rule('/', 'help', help)
@@ -53,19 +61,22 @@ def reroute_to_help():
 
 @app.route('/env/<envname>')
 def env(envname):
-    read_environments()
-    if envname not in environments:
+    environments.rereadIniFile()
+    if envname not in environments.getEnvironmentsList():
         return redirect(url_for('help'))
     else:
         rows = list()
-        # output = build_help_output()
-        nwm_url = environments[envname]
+        nwm_url = environments.getUrlForEnvironment(envname)
         try:
             response = requests.get(nwm_url, timeout=5)
         except requests.exceptions.Timeout:
             rows.append('ERROR: Timeout')
+            no_of_servers = 0
+            description='Failed to establish connection: timeout while connecting to "' + envname + '"'
         except requests.exceptions.ConnectionError:
             rows.append('ERROR: ConnectionError')
+            no_of_servers = 0
+            description='Failed to establish connection: url of "' + envname + '" not known'
         else:
             nwmlist = json.loads(response.text)
             for item in nwmlist:
@@ -77,8 +88,9 @@ def env(envname):
                     port=str(item.get('addresses')[0]['port'])
                     ))
             no_of_servers = len(rows)
-    return render_template("main.html", rows=rows, environments=environments, no_of_servers=no_of_servers)
+            description=environments.getEnvDescription(envname)
+    return render_template("main.html", rows=rows, environments=environments.getEnvironmentsList(), no_of_servers=no_of_servers, description=description)
 
 if __name__ == '__main__':
-    read_environments()
+    environments = Envs()
     app.run(debug=True, port=5000) #run app in debug mode on port 5000
